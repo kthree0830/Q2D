95409fd436635b8fc63725f9f57e480bd6a180d4
kthree0830 <yvkqw@qq.com>
2017-07-28 17:29:26 +0800

    裁剪

--- QDLearn/QDLearn/ViewController.m
+++ QDLearn/QDLearn/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    if ([self.title isEqualToString:@"裁剪"]) {
+        [self caijian];
+    }
 }
 
 
@@ -24,6 +26,28 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)caijian {
+    CGRect frame = {50,200,220,220};
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:frame];
+    [self.view addSubview:imageView];
+    
+    
+    CGFloat bordeW = 10;
+    UIImage *image = [UIImage imageNamed:@"tupian"];
+    CGSize size = CGSizeMake(image.size.width + 2*bordeW, image.size.width + 2*bordeW);
+    UIGraphicsBeginImageContext(size);
+//    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, size.width, size.height)];
+    [[UIColor yellowColor]setFill];
+    [path fill];
+    UIBezierPath *path1 = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(bordeW, bordeW, image.size.width, image.size.width)];
+    [path1 addClip];;
+    [image drawAtPoint:CGPointMake(bordeW, bordeW)];
+    UIImage *i = UIGraphicsGetImageFromCurrentImageContext();
+    
+    imageView.image = i;
+    UIGraphicsEndImageContext();
+}
 
 @end
